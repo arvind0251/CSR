5dad6d28abe412277e548d840f110d239e57bd64
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-04-17 17:29:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
-# bot.py
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 from handlers.commands import register_command_handlers
 from handlers.message_handler import register_message_handler
+from utils.error_handler import setup_global_exception_logger  # NEW LINE
+
+# Setup global error logging
+setup_global_exception_logger()  # NEW LINE
 
 app = Client("LearningBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
